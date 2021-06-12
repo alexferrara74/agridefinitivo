dcc4e8a0ec3e11efd4a2b7c4bc080574b777a295
Michele <russoantoniomichele9@gmail.com>
2021-06-12 18:06:21 +0200

    Pagina Admin iniziata

---
+++ AgriShop/WebContent/admin/profilo.jsp
@@ -0,0 +1,81 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1" import="model.*"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link href="../style/stylelogreg.css" rel="stylesheet" type="text/css">
+
+<script src="../JS/controllo-profilo.js"></script>
+
+<%
+String username= (String)request.getSession().getAttribute("nome");
+prodotto prod=(prodotto)request.getSession().getAttribute("prod");
+%>
+
+</head>
+<body>
+
+<div id="utente">
+<fieldset>
+<legend>Pagina di amministratore</legend>
+<p>Account di <%=username%></p>
+
+<form class="modprofilo"action="../AdminPage" method="post">
+
+<table>
+<!--  
+<tr>
+<td><input type="text" name="user " placeholder="<%=username%>"></td>
+</tr>
+-->
+<tr>
+<td><input type="text" name="prezzo" placeholder="<%=prod.getPrezzo()%>" readonly="readonly" ></td>
+</tr>
+<tr>
+
+<td><input type="text" name="nome" placeholder="<%=prod.getNome()%>"></td>
+</tr>
+<tr>
+
+<td><input type="text" name="Ssn" placeholder="<%=prod.getSsn()%>"></td>
+</tr>
+<tr>
+
+<td><input type="text" name="disponibilitÃ" placeholder="<%=prod.getQuantita()%>"></td>
+</tr>
+
+<tr>
+<td><input type="text" name="descrizione" placeholder="<%=prod.getDescrizione()%>"></td>
+</tr>
+
+<tr>
+<td><input type="text" name="categoria" placeholder="<%=prod.getCategoria()%>"></td>
+</tr>
+
+<tr>
+<td><input type="text" name="idfoto" placeholder="<%=prod.getIdfoto()%>"></td>
+</tr>
+
+<tr>
+<td><input type="password" name="password" placeholder="password" required></td>
+<td><input type="password" name="conferma" placeholder="conferma password" required></td>
+</tr>
+<tr>
+<td>
+<input type="submit" value="invio">
+</td>
+</tr>
+</table>
+</form>
+
+
+</fieldset>
+
+
+
+
+
+</div>
+</body>
+</html>
---
+++ AgriShop/src/servlet/AdminPage.java
@@ -0,0 +1,106 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import model.LoginModelDS;
+import model.Negozio;
+import model.ProductModelDS;
+import model.prodotto;
+import utils.utility;
+
+/**
+ * Servlet implementation class AdminPage
+ */
+@WebServlet("/AdminPage")
+public class AdminPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DataSource ds= (DataSource) getServletContext().getAttribute("DataSource");
+		ProductModelDS model= new ProductModelDS(ds);
+		prodotto prod=(prodotto) request.getSession().getAttribute("prod");
+		
+		String prezzo= request.getParameter("prezzo");
+		String nome=request.getParameter("nome");
+		String SSN=request.getParameter("Ssn");
+		String disponibilit√= request.getParameter("disponibilit√");
+		String descrizione=request.getParameter("descrizione");
+		String categ=request.getParameter("categ");
+		String idfoto=request.getParameter("idfoto");
+		
+		
+		
+			if(nome!=null) {			
+				if(!nome.equals("")) {
+					prod.setNome(nome);
+				}}
+		
+		if(SSN!=null) {			
+		if(!SSN.equals("")) {
+			prod.setSsn(SSN);
+		}
+		}
+	/*	if(disponibilit√!=null) {
+		if(!disponibilit√.equals("")){
+			prod.setQuantita(disponibilit√);;
+		}}*/
+		
+		if(descrizione!=null) {
+		if(!descrizione.equals("")) {
+			prod.setDescrizione(descrizione);
+		}}
+		
+		if(!categ.equals("")) {
+			prod.setCtegoria(categ);
+		}
+		
+	
+		try {
+			 model.doSave(prod);
+			
+		} catch (SQLException e) {
+			utility.print(e);
+			
+			request.setAttribute("error", e.getMessage());
+		}
+		
+		if(request.getSession() !=null) {
+			request.getSession().invalidate();
+		}
+		
+	
+		
+		RequestDispatcher dispacher=this.getServletContext().getRequestDispatcher("/Login.jsp");
+		dispacher.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
