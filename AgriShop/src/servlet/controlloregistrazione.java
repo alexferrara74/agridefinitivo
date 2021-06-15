845889b39371c672da9680c7c74bcc95a30912df
Marta <m.renzulli7@studenti.unisa.it>
2021-06-15 10:23:27 +0200

    immagini

---
+++ AgriShop/src/servlet/controlloregistrazione.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class controlloregistrazione
+ */
+@WebServlet("/controlloregistrazione")
+public class controlloregistrazione extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/xml");
+		String ragsociale= request.getParameter("ragionesociale");
+		
+			System.out.print(ragsociale);
+		response.getWriter().write("ciao");;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
